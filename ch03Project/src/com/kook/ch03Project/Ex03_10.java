3e8664795635602d99ab59358601ceb7489845f6
IT Recipe <40875025+itrecipe@users.noreply.github.com>
2022-12-22 13:49:31 +0900

    22.12.22
    java 수업

---
+++ ch03Project/src/com/kook/ch03Project/Ex03_10.java
@@ -0,0 +1,15 @@
+package com.kook.ch03Project;
+
+public class Ex03_10 {
+
+	public static void main(String[] args) {
+		long a = 1_000_000 * 1_000_000;
+		//1_000_000이 int이므로 int + int = int
+		long b = 1_000_000 * 1_000_000L;
+		//int * long -> long //정상
+
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+	}
+
+}
