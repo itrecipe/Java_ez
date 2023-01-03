892726ccb2b5bb0146fd4facfc38d24a82b64ec7
IT Recipe <40875025+itrecipe@users.noreply.github.com>
2023-01-03 09:55:08 +0900

    23.1.3
    java_ch06_quiz

--- ch06Project/src/com/kook/ch06Project/Ex06Quiz.java
+++ ch06Project/src/com/kook/ch06Project/Ex06Quiz.java
@@ -1,5 +1,4 @@
 package com.kook.ch06Project;
-
 //국쌤 퀴즈!
 public class Ex06Quiz {
 	//1. 인스턴스 변수로 문자열 변수 string
@@ -10,22 +9,37 @@ public class Ex06Quiz {
 	
 	//11. static블록 만들고 블록안의 실행문은 static출력
 	static {
-	System.out.println();	
+		System.out.println("static");
 	}
 	
 	//12. 인스턴스 블록 만들고 블록안의 실행문은 instance출력
+	//객체를 생성할때마다 먼저 실행후 생상자로 초기화
+	{
+		System.out.println("instance");
+	}
 	
 	public static void main(String[] args) {
-	
 		
 	//4. 3번의 메서드 호출
-		myMethod();
+		myMethod(); //클래스형 메서드내에서 동일클래스의 클래스형 메서드 호출시는
+		//클래스명.메서드()가 아닌 메서드명으로 호출
+		
 		//6. 5번의 메서드를 호출하는데 인자를 kook으로 해서 호출
+		myMethod("kook");
 		//8. 인스턴스메서드 instanceMethod를 호출하는데 인자로 "홍길동"을 사용
+		//동일 클레스이나 static메서드안에서 인스턴스 메서드 호출시 자기 클래스 객체를 만들어
+		//객체.인스턴스메서드(인자값); 이런식으로 호출
+		Ex06Quiz hgd = new Ex06Quiz();
+		hgd.instanceMethod("홍길동");
+		
 		//9. 인스턴스변수 string을 abcde를 초기화하고 출력
-			
+			//자기클래스의 인스턴스 변수도 클래스메서드안에서는 이름으로 접근이 안되고
+			//객체를 만들어 접근
+			hgd.string = "abcde";
+			System.out.println(hgd.string);
 		
 	//10. 클래스 변수 number를 30으로 초기화 하고 출력
+		//클래스 메서드안에서 자기클래스의 클래스 멤버변수는 객체.변수명으로도 접근 가능	
 		number = 30;
 		System.out.println(number);
 	}
@@ -44,7 +58,8 @@ public class Ex06Quiz {
 	
 	/* 7. 인스턴스 메서드인 이름 instanceMethod이고 리턴타입은 문자열이며
 		 파라미터는 문자열인 하나로 되어 있고 파라미터로 받은 값을 출력 하고 리턴값은 khs를 리턴 */
-	public String instanceMethod(String name) {
-		return name;
+	String instanceMethod(String name) {
+		System.out.println(name);
+		return "khs";
 	}
 }
