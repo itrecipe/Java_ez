ca64b97b893a9848be1cae9e95ef1a26a3d8f771
IT Recipe <itrecipe@naver.com>
2023-02-21 12:37:10 +0900

    23.2.21
    java & js 시험
    
    Signed-off-by: IT Recipe <itrecipe@naver.com>

---
+++ myProject1/src/com/ezen/myProject1/JavaStudyClass.java
@@ -0,0 +1,71 @@
+package com.ezen.myProject1;
+
+import java.util.Arrays;
+
+public class JavaStudyClass {
+	//5장-배열(시험) 언어의 특성 활용
+	public static void main(String[] args) {
+	
+	//가
+		String[] cars1 = {"Volvo","BMW","Ford","Mazda"};
+		//나
+		String[] cars2 = new String[]{"Volvo","BMW","Ford","Mazda"};
+		//다
+		String[] cars3 = new String[4];
+		cars3 [0] = "Volvo";
+		cars3 [1] = "BMW";
+		cars3 [2] = "Ford";
+		cars3 [3] = "Mazda";
+		//라
+		for(int i=0; i < cars1.length; i++) {
+			System.out.println("cars1 배열의 요소 출력 (기본 for) : " + cars1[i]);
+		}
+		//마
+		for(String str:cars2) { //새로운 for문
+			System.out.println("(마) enhanced for문 : " + str);
+			
+		}
+		//바
+		int[][] myNumbers = {
+				{1,2,3,4},
+				{5,6,7}
+		};
+		//사
+		System.out.println("(사) : " + (myNumbers[1][1]));
+		
+		//아
+		for(int j=0; j < myNumbers.length; j++) {
+			System.out.println("(아) myNumbers 배열의 요소값 : " + Arrays.deepToString(myNumbers));
+		}
+		
+		//타
+		Animal animal1 = new Animal();
+		Animal animal2 = new Animal();
+		
+		//파
+		animal1.kind = "bird";
+		animal1.age = 10;
+		animal1.name = "puppy";
+		
+		animal2.kind = "fish";
+		animal2.age = 15;
+		animal2.name = "kalchi";
+		
+		//하
+		animal1.setAge(10);
+		animal2.setAge(15);
+	}
+}
+
+//자
+class Animal {
+	//차
+	String kind;
+	int age;
+	String name;
+	
+	//카
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
