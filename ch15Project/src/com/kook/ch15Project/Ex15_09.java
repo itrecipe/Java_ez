99d482c23b1c9302e1f9eae5918a6a0ea87e25f4
IT Recipe <itrecipe@naver.com>
2023-02-28 13:48:02 +0900

    java_exam
    23.2.28
    
    Signed-off-by: IT Recipe <itrecipe@naver.com>

---
+++ ch15Project/src/com/kook/ch15Project/Ex15_09.java
@@ -0,0 +1,24 @@
+package com.kook.ch15Project;
+
+import java.io.*;
+
+public class Ex15_09 {
+
+	public static void main(String[] args) {
+		try {
+		FileReader fr = new FileReader(args[0]);
+		FileWriter fw = new FileWriter(args[1]);
+		
+		int data = 0;
+		while((data = fr.read()) != -1) {
+			fw.write(data); //출력 FileWriter객체에 써준다(즉, 출력 파일에 작성한다는 의미)
+		}
+		
+		fr.close();
+		fw.close();
+	}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
