93c7eef3ed718fe3a14880cbaab606c23e2604da
IT Recipe <itrecipe@naver.com>
2023-08-31 00:35:21 +0900

    java_exam
    23.8.31

---
+++ java_ez/src/com/h/test/GuGuDan2.java
@@ -0,0 +1,24 @@
+package com.h.test;
+
+public class GuGuDan2 {
+
+	public static void main(String[] args) {
+		
+		//정수 n을 입력 받아서 구구단을 출력하라
+		
+		/*
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		*/
+		
+		System.out.println("구구단 출력!");
+
+		for(int i=2; i<10; i++) {
+			System.out.println();
+			for(int j=1; i<10; j++) {
+				System.out.println(i + " x " + j + " = " + i * j);
+			}
+		}
+		System.out.println("시스템을 종료 합니다.");
+	}
+}
