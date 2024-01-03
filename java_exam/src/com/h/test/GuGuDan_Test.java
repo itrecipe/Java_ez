572d5fa3a5416992b9aeeb2253b829a70ad78aaa
IT Recipe <itrecipe@naver.com>
2024-01-03 19:04:58 +0900

    Java_exam
    학습

--- java_exam/src/com/h/test/GuGuDan_Test.java
+++ java_exam/src/com/h/test/GuGuDan_Test.java
@@ -1,12 +1,15 @@
 package com.h.test;
 
-public class GuGuDan {
+public class GuGuDan_Test {
 
 	public static void main(String[] args) {
 
+		System.out.println("구구단 출력!");
+		
 		for(int i=2; i<10; i++) {
+			
 			System.out.println();
-			System.out.println(i + "단" + " 출력");
+			System.out.println(i + " 단 출력");
 			System.out.println();
 			
 			for(int j=1; j<10; j++) {
