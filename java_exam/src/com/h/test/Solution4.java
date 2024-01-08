77a72959d12408080eecee3b8cd80ced0c5db0fa
IT Recipe <itrecipe@naver.com>
2024-01-08 15:05:50 +0900

    Java_Study
    24.01.08 학습

---
+++ java_exam/src/com/h/test/Solution4.java
@@ -0,0 +1,15 @@
+package com.h.test;
+
+import java.util.Scanner;
+
+//문자열 출력하기
+public class Solution4 {
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("문자를 입력 하시오");
+	     String a = sc.next();
+	     a = "HelloWorld!";
+	     System.out.println(a);
+	     
+	}
+}
