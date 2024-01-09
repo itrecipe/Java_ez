aa32fa5638805b7fec439c27af1d756ceacd8138
IT Recipe <itrecipe@naver.com>
2024-01-09 16:31:34 +0900

    Java_exam
    24.01.09 학습

--- java_exam/src/com/h/test/Solution3.java
+++ java_exam/src/com/h/test/Solution3.java
@@ -3,7 +3,7 @@ package com.h.test;
 import java.util.Scanner;
 
 //문자열 출력하기
-public class Solution4 {
+public class Solution3 {
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
 		 System.out.println("문자를 입력 하시오");
